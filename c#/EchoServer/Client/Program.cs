28501e5002e515e9914c25f66dfc7d657116e2a9
ssaturn <ssaturn@dreamwiz.com>
2018-09-05 15:28:43 +0900

    c# echo server 테스트

---
+++ c#/EchoServer/Client/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoClient
+{
+    class Client
+    {
+        public static void Connect()
+        {
+            IPHostEntry host_entry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress address = host_entry.AddressList.First(a => a.AddressFamily == AddressFamily.InterNetworkV6);
+            IPEndPoint end_point = new IPEndPoint(address, 9090);
+
+            Socket sender = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(end_point);
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint);
+
+                byte[] data = Encoding.ASCII.GetBytes("Hello World!");
+                Console.WriteLine("Sent : Hello World!");
+                int byte_sent = sender.Send(data);
+
+                string msg = null;
+                while(true)
+                {
+                    byte[] buffer = new byte[1024];
+
+                    int bytes_recived = sender.Receive(buffer);
+                    if (bytes_recived > 0 )
+                    {
+                        msg += Encoding.ASCII.GetString(buffer, 0, bytes_recived);
+                    }
+
+                    if(msg.Contains(""))
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Received : " + msg);
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client.Connect();
+        }
+    }
+}
---
+++ c#/EchoServer/ConsoleApp1/Server.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace EchoServer
+{
+    class Server
+    {
+        public static void Listen()
+        {
+            IPHostEntry host_entry = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress address = host_entry.AddressList.First(a => a.AddressFamily == AddressFamily.InterNetworkV6);
+            IPEndPoint local_end_point = new IPEndPoint(address, 9090);
+
+            Socket listener = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(local_end_point);
+                listener.Listen(10);
+
+                while(true)
+                {
+                    Console.WriteLine("Waiting for a connections ... ");
+                    Socket handler = listener.Accept();
+                    string msg = null;
+
+                    while(true)
+                    {
+                        byte[] buffer = new byte[1024];
+                        int byte_count = handler.Receive(buffer);
+
+                        if( byte_count > 0 )
+                        {
+                            msg += Encoding.ASCII.GetString(buffer, 0, byte_count);
+                        }
+
+                        if ( msg.Contains(""))
+                        {
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine("Received : {0}", msg);
+
+                    byte[] data = Encoding.ASCII.GetBytes(msg);
+
+                    Console.WriteLine("Send : {0}", msg);
+                    handler.Send(data);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
